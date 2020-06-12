9b0251599d4566a1af416db0cdd6a897e923032b
Javix12 <javix12_chachin@hotmail.com>
2020-06-11 21:35:04 -0500

    Rip

---
+++ database/db.sql
@@ -0,0 +1,12 @@
+CREATE TABLE alumno (
+  IdAlumno INT(8) NOT NULL,
+  NoControl INT(8)  NOT NULL,
+  username VARCHAR(100) NOT NULL,
+  fullname VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  Semestre INT(12) NOT NULL,
+  Carrera VARCHAR(50) NOT NULL,
+  password VARCHAR(60) NOT NULL
+);
+
+
